eea8adb10d96adacf8a7220f89df0f41bbf24313
Dineth De Silva <152493468+dinethde@users.noreply.github.com>
2026-07-14 12:58:22 +0530

    Add: two-integer-sum - submission-19

---
+++ Data Structures & Algorithms/two-integer-sum/submission-19.ts
@@ -0,0 +1,17 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number[]}
+     */
+    twoSum(nums: number[], target: number): number[] {
+        const numsMap = new Map();
+        for (let i = 0; i < nums.length; i++){
+            if (numsMap.get(nums[i]) !== undefined) {
+                return [numsMap.get(nums[i]), i]
+            }
+
+            numsMap.set(target - nums[i], i);
+        }
+    }
+}
