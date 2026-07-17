dc74951d9e569751427ee57eefcd9079c2a0b124
Dineth De Silva <152493468+dinethde@users.noreply.github.com>
2026-07-17 10:53:36 +0530

    Add: top-k-elements-in-list - submission-9

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-9.ts
@@ -0,0 +1,23 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} k
+     * @return {number[]}
+     */
+    topKFrequent(nums: number[], k: number): number[] {
+        const map = new Map();
+        const freq = [];
+        for (let i = 0; i < nums.length; i++){
+            map.set(nums[i], (map.get(nums[i]) || 0 ) +1);
+        }
+
+        const sortedMap = new Map([...map].sort(((a, b) => b[1] - a[1])))
+
+        const res = []
+        for (let i=0; i<k; i++){
+            res.push([...sortedMap][i][0])
+        }
+
+        return res;
+    }
+}
